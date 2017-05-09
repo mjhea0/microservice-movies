d045bf0ba4c0540952025d7eafd651236dca5427
Michael Herman <hermanmu@gmail.com>
2017-05-09 14:09:53 -0600

    added swager

---
+++ services/movies/swagger/index.js
@@ -0,0 +1,19 @@
+const app = require('connect')();
+const http = require('http');
+const swaggerTools = require('swagger-tools');
+const jsyaml = require('js-yaml');
+const fs = require('fs');
+
+const serverPort = 3001;
+const options = { swaggerUi: '/swagger.json' };
+
+const spec = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDoc = jsyaml.safeLoad(spec);
+
+swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
+  app.use(middleware.swaggerMetadata());
+  app.use(middleware.swaggerValidator());
+  app.use(middleware.swaggerRouter(options));
+  app.use(middleware.swaggerUi());
+  http.createServer(app).listen(serverPort);
+});
