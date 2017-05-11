2e536e969730228ba220bdc20d20986869e22b0f
Michael Herman <hermanmu@gmail.com>
2017-05-11 16:24:19 -0600

    updated tavis

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 docker-compose run users-service knex migrate:latest --env development --knexfile app/knexfile.js
 docker-compose run users-service knex seed:run --env development --knexfile app/knexfile.js
