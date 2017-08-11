0fd7d45214b7e02dc9862d056784a39812b7a864
Michael Herman <hermanmu@gmail.com>
2017-08-11 15:43:11 -0600

    updated slides

--- services/web/src/App.jsx
+++ services/web/src/App.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { Route, Redirect, Switch, Link } from 'react-router-dom'
 import axios from 'axios'
 
-const API_URL = 'http://www.omdbapi.com/?s=';
+const API_URL = 'http://www.omdbapi.com/?apikey=c5a8df09&s=';
 
 import './App.css';
 
