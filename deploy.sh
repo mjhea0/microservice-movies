18204376f9842032672690872fb074047aa3d03c
Michael Herman <hermanmu@gmail.com>
2017-09-04 08:53:18 -0600

    added tagging and pushing of images to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,9 @@ ECS_REGION="us-east-1"
 ECS_CLUSTER="review"
 VPC_ID="vpc-26ba875f"
 LOAD_BALANCER_LISTENER_ARN="arn:aws:elasticloadbalancing:us-east-1:046505967931:loadbalancer/app/ecs/1dec50e459068da0"
+NAMESPACE="sample"
+IMAGE_BASE="microservicemovies"
+ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com"
 
 
 # helpers
@@ -36,8 +39,28 @@ get_cluster() {
   fi
 }
 
+tag_and_push_images() {
+	echo "Tagging and pushing images..."
+	$(aws ecr get-login --region "${ECS_REGION}")
+	# tag
+	docker tag ${IMAGE_BASE}_users-db-review ${ECR_URI}/${NAMESPACE}/users-db-review:review
+	docker tag ${IMAGE_BASE}_movies-db-review ${ECR_URI}/${NAMESPACE}/movies-db-review:review
+	docker tag ${IMAGE_BASE}_users-service-review ${ECR_URI}/${NAMESPACE}/users-service-review:review
+	docker tag ${IMAGE_BASE}_movies-service-review ${ECR_URI}/${NAMESPACE}/movies-service-review:review
+	docker tag ${IMAGE_BASE}_web-service-review ${ECR_URI}/${NAMESPACE}/web-service-review:review
+	docker tag ${IMAGE_BASE}_swagger-review ${ECR_URI}/${NAMESPACE}/swagger-review:review
+	# push
+	docker push ${ECR_URI}/${NAMESPACE}/users-db-review:review
+	docker push ${ECR_URI}/${NAMESPACE}/movies-db-review:review
+	docker push ${ECR_URI}/${NAMESPACE}/users-service-review:review
+	docker push ${ECR_URI}/${NAMESPACE}/movies-service-review:review
+	docker push ${ECR_URI}/${NAMESPACE}/web-service-review:review
+	docker push ${ECR_URI}/${NAMESPACE}/swagger-review:review
+	echo "Images tagged and pushed!"
+}
 
 # main
 
 configure_aws_cli
 get_cluster
+tag_and_push_images
