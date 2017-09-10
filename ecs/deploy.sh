280ba6bfab3a0e5aba5952bf9e5e78215e30ba24
Michael Herman <hermanmu@gmail.com>
2017-09-10 11:57:09 -0600

    tag and push images

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -15,6 +15,11 @@ ECS_CLUSTER="review"
 VPC_ID="vpc-0f6d6f76"
 LOAD_BALANCER_ARN="arn:aws:elasticloadbalancing:us-east-1:046505967931:loadbalancer/app/ecs/13e936f9fca7fa49"
 SAMPLE_TARGET_GROUP_ARN="arn:aws:elasticloadbalancing:us-east-1:046505967931:listener/app/ecs/13e936f9fca7fa49/9b29b63d41927a83"
+NAMESPACE="sample"
+IMAGE_BASE="microservicemovies"
+ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com"
+SHORT_GIT_HASH=$(echo $CIRCLE_SHA1 | cut -c -7)
+TAG=$SHORT_GIT_HASH
 
 # helpers
 
@@ -34,6 +39,26 @@ get_cluster() {
       echo "Error finding cluster."
       return 1
   fi
+}
+
+tag_and_push_images() {
+  echo "Tagging and pushing images..."
+  $(aws ecr get-login --region "${ECS_REGION}")
+  # tag
+  docker tag ${IMAGE_BASE}_users-db-review ${ECR_URI}/${NAMESPACE}/users-db-review:${TAG}
+  docker tag ${IMAGE_BASE}_movies-db-review ${ECR_URI}/${NAMESPACE}/movies-db-review:${TAG}
+  docker tag ${IMAGE_BASE}_users-service-review ${ECR_URI}/${NAMESPACE}/users-service-review:${TAG}
+  docker tag ${IMAGE_BASE}_movies-service-review ${ECR_URI}/${NAMESPACE}/movies-service-review:${TAG}
+  docker tag ${IMAGE_BASE}_web-service-review ${ECR_URI}/${NAMESPACE}/web-service-review:${TAG}
+  docker tag ${IMAGE_BASE}_swagger-review ${ECR_URI}/${NAMESPACE}/swagger-review:${TAG}
+  # push
+  docker push ${ECR_URI}/${NAMESPACE}/users-db-review:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/movies-db-review:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/users-service-review:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/movies-service-review:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/web-service-review:${TAG}
+  docker push ${ECR_URI}/${NAMESPACE}/swagger-review:${TAG}
+  echo "Images tagged and pushed!"
 }
 
 
