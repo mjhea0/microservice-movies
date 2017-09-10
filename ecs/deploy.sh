23c2270c91616cfdb2b41deb9862cabcc31a8c5a
Michael Herman <hermanmu@gmail.com>
2017-09-10 13:59:16 -0600

    create listener

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -14,7 +14,7 @@ ECS_REGION="us-east-1"
 ECS_CLUSTER="review"
 VPC_ID="vpc-0f6d6f76"
 LOAD_BALANCER_ARN="arn:aws:elasticloadbalancing:us-east-1:046505967931:loadbalancer/app/ecs/13e936f9fca7fa49"
-SAMPLE_TARGET_GROUP_ARN="arn:aws:elasticloadbalancing:us-east-1:046505967931:listener/app/ecs/13e936f9fca7fa49/9b29b63d41927a83"
+SAMPLE_TARGET_GROUP_ARN="aarn:aws:elasticloadbalancing:us-east-1:046505967931:targetgroup/sample/d22b8bbb71dc2a45"
 NAMESPACE="sample"
 IMAGE_BASE="microservicemovies"
 ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com"
@@ -74,6 +74,7 @@ create_task_defs() {
   register_definition
   create_target_group "users" "3000" "/users/ping"
   get_target_group_arn "users"
+  add_rules "1" "/users*"
   # movies
   echo "Creating movies task definition..."
   family="sample-movies-review-td"
@@ -85,6 +86,7 @@ create_task_defs() {
   register_definition
   create_target_group "movies" "3000" "/movies/ping"
   get_target_group_arn "movies"
+  add_rules "2" "/movies*"
   # web
   echo "Creating web task definition..."
   family="sample-web-review-td"
@@ -96,6 +98,7 @@ create_task_defs() {
   register_definition
   create_target_group "web" "9000" "/"
   get_target_group_arn "web"
+  add_rules "3" "/"
 }
 
 register_definition() {
@@ -129,9 +132,46 @@ get_target_group_arn() {
   fi
 }
 
+get_listener_port() {
+  echo "Getting listener port..."
+  if port=$(aws elbv2 describe-listeners --load-balancer-arn $LOAD_BALANCER_ARN | $JQ ".Listeners | max_by(.Port) | .Port"); then
+      if [[ port == "80" ]]; then
+          port=30000
+      else
+          port=$(($port+1))
+      fi
+    echo "Listener port: $port"
+  else
+    echo "Failed to get listener port."
+    return 1
+  fi
+}
+
+create_listener() {
+  echo "Creating listener..."
+  if load_balancer_listener_arn=$(aws elbv2 create-listener --load-balancer-arn $LOAD_BALANCER_ARN --protocol HTTP --port $port --default-actions Type=forward,TargetGroupArn=$SAMPLE_TARGET_GROUP_ARN | $JQ ".Listeners[0].ListenerArn"); then
+      echo "Listener created - $load_balancer_listener_arn"
+  else
+      echo "Error creating listener."
+      return 1
+  fi
+}
+
+add_rules() {
+  echo "Add rules..."
+  if [[ $(aws elbv2 create-rule --listener-arn $load_balancer_listener_arn --priority $1 --conditions Field=path-pattern,Values="$2" --actions Type=forward,TargetGroupArn=$target_group_arn | $JQ ".Rules[0].Actions[0].TargetGroupArn") == $target_group_arn ]]; then
+      echo "Rules created!"
+  else
+      echo "Error creating rule."
+      return 1
+  fi
+}
+
 # main
 
 configure_aws_cli
 get_cluster
 tag_and_push_images
+get_listener_port
+create_listener
 create_task_defs
