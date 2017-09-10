2f23a3b45752038d3ba6acc334cdf511c48cb985
Michael Herman <hermanmu@gmail.com>
2017-09-10 17:05:38 -0600

    create services

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -21,6 +21,8 @@ ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${ECS_REGION}.amazonaws.com"
 SHORT_GIT_HASH=$(echo $CIRCLE_SHA1 | cut -c -7)
 TAG=$SHORT_GIT_HASH
 TARGET_GROUP=$SHORT_GIT_HASH
+ECS_SERVICE=$SHORT_GIT_HASH
+
 
 # helpers
 
@@ -75,6 +77,11 @@ create_task_defs() {
   create_target_group "users" "3000" "/users/ping"
   get_target_group_arn "users"
   add_rules "1" "/users*"
+  service_template_name="users-review_service.json"
+  service_template=$(cat "ecs/services/$service_template_name")
+  service=$(printf "$service_template" $ECS_CLUSTER "$ECS_SERVICE-users" $revision $target_group_arn)
+  echo "$service"
+  create_service
   # movies
   echo "Creating movies task definition..."
   family="sample-movies-review-td"
@@ -87,6 +94,11 @@ create_task_defs() {
   create_target_group "movies" "3000" "/movies/ping"
   get_target_group_arn "movies"
   add_rules "2" "/movies*"
+  service_template_name="movies-review_service.json"
+  service_template=$(cat "ecs/services/$service_template_name")
+  service=$(printf "$service_template" $ECS_CLUSTER "$ECS_SERVICE-movies" $revision $target_group_arn)
+  echo "$service"
+  create_service
   # web
   echo "Creating web task definition..."
   family="sample-web-review-td"
@@ -99,6 +111,11 @@ create_task_defs() {
   create_target_group "web" "9000" "/"
   get_target_group_arn "web"
   add_rules "3" "/"
+  service_template_name="web-review_service.json"
+  service_template=$(cat "ecs/services/$service_template_name")
+  service=$(printf "$service_template" $ECS_CLUSTER "$ECS_SERVICE-web" $revision $target_group_arn)
+  echo "$service"
+  create_service
 }
 
 register_definition() {
@@ -163,6 +180,16 @@ add_rules() {
       echo "Rules created!"
   else
       echo "Error creating rule."
+      return 1
+  fi
+}
+
+create_service() {
+  echo "Creating service..."
+  if [[ $(aws ecs create-service --cli-input-json "$service" | $JQ ".service.taskDefinition") == $revision ]]; then
+      echo "Service created!"
+  else
+      echo "Error creating service."
       return 1
   fi
 }
