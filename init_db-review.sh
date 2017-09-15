8f0d53977fac36e14387a81665d5ceb101b9bc35
Michael Herman <hermanmu@gmail.com>
2017-09-15 13:36:18 -0600

    added entrypoint file, updated review dockerfiles

--- init_db-review.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose-review.yml run users-service-review knex migrate:latest --env test --knexfile app/knexfile.js
-docker-compose -f docker-compose-review.yml run users-service-review knex seed:run --env test --knexfile app/knexfile.js
-docker-compose -f docker-compose-review.yml run movies-service-review knex migrate:latest --env test --knexfile app/knexfile.js
-docker-compose -f docker-compose-review.yml run movies-service-review knex seed:run --env test --knexfile app/knexfile.js
